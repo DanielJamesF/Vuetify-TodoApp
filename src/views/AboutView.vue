04ced06507fc3b01da51082e50b70bf08fa70ba8
DanielJamesF <fredericksd17@gmail.com>
2022-09-27 15:01:20 +0200

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur nam, ipsa nulla tempora placeat recusandae
+      vero, repudiandae a explicabo laudantium dolor iure earum ad eaque aspernatur quasi culpa quia. Eveniet?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur nam, ipsa nulla tempora placeat recusandae
+      vero, repudiandae a explicabo laudantium dolor iure earum ad eaque aspernatur quasi culpa quia. Eveniet?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur nam, ipsa nulla tempora placeat recusandae
+      vero, repudiandae a explicabo laudantium dolor iure earum ad eaque aspernatur quasi culpa quia. Eveniet?</p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="home">
+    <v-list
+    class="pt-0" 
+    flat>
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item @click="doneTask(task.id)">
+        <template v-slot:default>
+          <v-list-item-action>
+            <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ task.title }}</v-list-item-title>
+          </v-list-item-content>
+        </template>
+      </v-list-item>
+      <v-divider></v-divider>
+    </div>
+    </v-list>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'todo',
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: 'Wake up',
+          done: false
+        },
+        {
+          id: 2,
+          title: 'Get bananas',
+          done: false
+        },
+        {
+          id: 3,
+          title: 'Eat bananas',
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+    doneTask(id) {
+      console.log('id: ', id)
+      let task = this.tasks.filter(task => task.id === id)[0]
+      task.done = !task.done
+    }
+  }
+});
+</script>
